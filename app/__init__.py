51cf8bc35dc7a55867f873c689d2a4deffaf0c40
mjp2220 <mjp2220@columbia.edu>
2015-02-06 23:09:58 -0500

    Adding the data API

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,35 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, Response
+from pymongo import MongoClient
+import json, datetime
 
 app = Flask(__name__)
 
 app.config.from_envvar('APP_SETTINGS')
 
+DATABASE = 'currency'
+
+from data.controllers import data as data_module
+app.register_blueprint(data_module)
+
 @app.route("/")
 def index():
   return render_template("index.html")
+
+@app.route("/timedata/<to_curr>/<from_curr>/")
+def timedata(to_curr, from_curr):
+  client = MongoClient()
+  currency = client[DATABASE].points
+  data_points = currency.find(
+    { 
+      "to": to_curr,
+      "from": from_curr
+    }
+  )
+  dthandler = lambda obj: (obj.isoformat() if isinstance(obj, datetime.datetime)
+    or isinstance(obj, datetime.date)
+    else None)
+  points = {}
+  return Response(json.dumps(list(data_points), default=dthandler), mimetype='application/json')
 
 @app.errorhandler(404)
 def not_found(error):
---
+++ app/data/controllers.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, Response, jsonify
+from pymongo import MongoClient
+import json, datetime
+
+DATABASE = 'currency'
+
+data = Blueprint('data', __name__, url_prefix='/data')
+
+@data.route("/series/<to_curr>/<from_curr>")
+def series(to_curr, from_curr):
+  client = MongoClient()
+  currency = client[DATABASE].points
+  data_points = currency.find(
+    { 
+      "to" : to_curr,
+      "from" : from_curr
+    }
+  )
+  dthandler = lambda obj: (obj.isoformat() if isinstance(obj, datetime.datetime)
+    or isinstance(obj, datetime.date)
+    else None)
+  points = {}
+  return Response(json.dumps(list(data_points), default=dthandler), mimetype='application/json')
+
+@data.route("/statistics/<to_curr>/<from_curr>")
+def statistics(to_curr, from_curr):
+  client = MongoClient()
+  currency = client[DATABASE].statistics
+  data_points = currency.find(
+    {
+      "to" : to_curr,
+      "from" : from_curr
+    }
+  )
+  return Response(unserialize(data_points), mimetype='application/json')
+
+def unserialize(data_points):
+  dthandler = lambda obj: (obj.isoformat() if isinstance(obj, datetime.datetime)
+    or isinstance(obj, datetime.date)
+    else None)
+  return json.dumps(list(data_points), default=dthandler)
