bb99f76e71b11105fc95b1dd62e700791076d9a0
Matthew Piccolella <mjp2220@columbia.edu>
2015-02-07 11:20:01 +0000

    Finishing up the base demo

--- app/data/controllers.py
+++ app/data/controllers.py
@@ -26,16 +26,25 @@ def series(to_curr, from_curr):
 def statistics(to_curr, from_curr):
   client = MongoClient()
   currency = client[DATABASE].statistics
-  data_points = currency.find(
+  response = {}
+  statistics = currency.find(
+    {
+      "to": to_curr,
+      "from": from_curr
+  })
+  response['statistics'] = list(statistics)
+  data_points = client[DATABASE].points.find(
     {
       "to" : to_curr,
       "from" : from_curr
     }
-  )
-  return Response(unserialize(data_points), mimetype='application/json')
+  ).sort("date", -1).limit(1)
+  response['latest'] = list(data_points)
+  print response
+  return Response(unserialize(response), mimetype='application/json')
 
 def unserialize(data_points):
   dthandler = lambda obj: (obj.isoformat() if isinstance(obj, datetime.datetime)
     or isinstance(obj, datetime.date)
     else None)
-  return json.dumps(list(data_points), default=dthandler)
+  return json.dumps(data_points, default=dthandler)
